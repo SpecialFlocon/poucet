ce610530e925321afcbcc18c52daf177b972d134
SpécialFlocon <specialflocon@protonmail.com>
2022-07-30 23:22:50 +0200

    The ping command does not need guild config

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -3,17 +3,7 @@ use crate::{Context, Error};
 /// Am I responding? Use this command to find out!
 #[poise::command(slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let serves_guild = ctx.data().serves_guild(ctx.guild_id().unwrap()).await?;
-
-    if !serves_guild {
-        poise::send_reply(ctx, |reply| {
-            reply
-                .content("I am not configured to work on this server! Have an admin configure me using the /setup command.")
-                .ephemeral(true)
-        }).await?;
-    } else {
-        ctx.say("Coin !").await?;
-    }
+    ctx.say("Pong!").await?;
 
     Ok(())
 }
